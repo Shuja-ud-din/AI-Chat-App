71407703f2aed2335ba12b6ef585414532152564
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-07-30 04:31:39 -0400

    converting in tailwind css + responsiveness

--- src/components/ChatQuickActions.tsx
+++ src/components/ChatQuickActions.tsx
@@ -1,4 +1,3 @@
-import "../App";
 import EnterIcon from "../assets/icons/enter-icon.svg";
 
 interface IProps {
@@ -13,12 +12,21 @@ const ChatQuickActions = ({ quickActions }: IProps) => {
     console.log("action => ", action);
   };
   return (
-    <div className="btn-section">
+    <div className="flex flex-wrap gap-[4rem] lg:mx-[3rem] my-[1.5rem] w-full sm:mx-[0rem]">
+
       {quickActions.map((action) => (
-        <div className="individual-btn-section" onClick={handleActiveAction}>
+        <div
+          key={action.title}
+          className="flex justify-between items-center mt-4 p-8 lg:w-1/4  sm:w-full rounded-3xl bg-white"
+          onClick={handleActiveAction}
+        >
           <div className="content">
-            <h3>{action.title}</h3>
-            <p>{action.description}</p>
+            <h3 className="bg-clip-text text-transparent bg-gradient-to-b from-[#6e6cc8] to-[#7bd8c3] text-2xl font-semibold mb-2.5">
+              {action.title}
+            </h3>
+            <p className="text-lg font-medium text-[#9fa0a6]">
+              {action.description}
+            </p>
           </div>
           <div className="enter-btn">
             <img src={EnterIcon} alt="" />
@@ -28,4 +36,5 @@ const ChatQuickActions = ({ quickActions }: IProps) => {
     </div>
   );
 };
+
 export default ChatQuickActions;
